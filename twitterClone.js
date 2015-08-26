e26227396d3b0e82093d4d77f65b6dc10a6a6789
sfny <sunrisefade+github@gmail.com>
2015-08-26 13:42:45 -0700

    acounts added

--- twitterClone.js
+++ twitterClone.js
@@ -1,18 +1,82 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Tweets = new Meteor.Collection("tweets");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+
+  Template.userManagement.helpers({
+
+  });
+
+  Template.userManagement.events({
+    'click #signup': function() {
+      var user = {
+        username: $('#signup-username').val(),
+        password: $('#signup-password').val(),
+        profile: {
+          fullname: $('#signup-fullname').val()
+        }
+      };
+
+      Accounts.createUser(user, function (error) {
+        if(error) alert(error);
+      });
+    },
+
+    'click #login': function() {
+      var username = $('#login-username').val();
+      var password = $('#login-password').val();
+
+      Meteor.loginWithPassword(username, password, function(error) {
+        if(error) alert(error);
+      });
+    },
+
+    'click #logout': function() {
+      Meteor.logout();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+
+
+
+  Template.tweetBox.helpers({
+    charCount: function() {
+      return 140 - Session.get('numChars');
+    },
+
+    charClass: function() {
+      if (Session.get('numChars') > 140) {
+        return 'errCharCount';
+      } else {
+        return 'charCount';
+      }
+    },
+
+    disableButton: function() {
+      if (Session.get('numChars') <= 0 ||
+          Session.get('numChars') > 140 ||
+          !Meteor.user()) {
+        return 'disabled';
+      }
     }
+  });
+
+  Template.tweetBox.events({
+    'input #tweetText': function(){
+      Session.set('numChars', $('#tweetText').val().length);
+    },
+
+    'click button': function() {
+      var tweet = $('#tweetText').val();
+      $('#tweetText').val("");
+      Session.set('numChars', 0);
+      if (Meteor.user()) {
+        Tweets.insert({message: tweet, user: Meteor.user().username});
+      }
+    }
+  });
+
+  Template.tweetBox.onRendered(function () {
+    Session.set('numChars', 0);
   });
 }
 
